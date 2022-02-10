60983a6386aaaada49e49cb9728a795b39669e58
Vincent <vincentgauthier068@gmail.com>
2022-02-09 22:15:47 -0800

    table mongoDB until search is implemented

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import Layout from "../components/Layout";
 import TextAnimation from "../components/animations/TextAnimation";
 import FadeIn from "../components/animations/FadeIn";
+import { MoviePlay, Tv, Game } from "styled-icons/boxicons-regular";
 
 const Hero = styled.div`
   margin: auto;
@@ -140,21 +141,21 @@ export default function Home({ isConnected }) {
   );
 }
 
-export async function getServerSideProps(context) {
-  try {
-    // client.db() will be the default database passed in the MONGODB_URI
-    // You can change the database by calling the client.db() function and specifying a database like:
-    // const db = client.db("myDatabase");
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
-    await clientPromise;
-    return {
-      props: { isConnected: true },
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      props: { isConnected: false },
-    };
-  }
-}
+// export async function getServerSideProps(context) {
+//   try {
+//     // client.db() will be the default database passed in the MONGODB_URI
+//     // You can change the database by calling the client.db() function and specifying a database like:
+//     // const db = client.db("myDatabase");
+//     // Then you can execute queries against your database like so:
+//     // db.find({}) or any of the MongoDB Node Driver commands
+//     await clientPromise;
+//     return {
+//       props: { isConnected: true },
+//     };
+//   } catch (e) {
+//     console.error(e);
+//     return {
+//       props: { isConnected: false },
+//     };
+//   }
+// }
